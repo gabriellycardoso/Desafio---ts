05a5f79a41d8014b7f554aa7b6fd02c779a3db4b
Gabrielly Cardoso <gabriellybrancocardoso@gmail.com>
2022-10-14 16:04:45 -0300

    first commit

---
+++ Personagem.js
@@ -0,0 +1,6 @@
+"use strict";
+let valor = [10, 12, 14, 18, 20];
+let geral = 24;
+for (let index = 0; index < valor.length; index++) {
+    console.log(geral + valor[index]);
+}
---
+++ Personagem.ts
@@ -0,0 +1,9 @@
+let valor: number [] = [10, 12, 14, 18, 20]
+
+let geral: number = 24;
+for (let index = 0; index < valor.length; index++){
+    console.log(geral + valor[index])
+}
+
+
+
